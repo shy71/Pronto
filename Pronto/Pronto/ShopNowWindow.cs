9b4ca9ca958c570e64366258a1ea276cb6a5050f
Shy Tennenbaum <shytennenbaum@gmail.com>
2016-12-27 20:53:07 +0200

    added ShopNow

---
+++ Pronto/Pronto/ShopNowWindow.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Pronto
+{
+    [Activity(Label = "ShopNowWindow")]
+    public class ShopNowWindow : Activity
+    {
+        ListView listView;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.ShopNowWindow);
+            LinearLayout item1 = (LinearLayout)((LayoutInflater)this.GetSystemService(Context.LayoutInflaterService)).Inflate(Resource.Layout.ListItem, null);
+            LinearLayout item2 = (LinearLayout)((LayoutInflater)this.GetSystemService(Context.LayoutInflaterService)).Inflate(Resource.Layout.ListItem, null); 
+
+            LinearLayout main=(LinearLayout)FindViewById(Resource.Id.main);
+            TextView text = new TextView(Application.Context);
+            text.Text = "shya";
+            main.AddView(item1);
+            main.AddView(item2);
+           // listView.AddView(item);
+
+        }
+    }
+    
+}
