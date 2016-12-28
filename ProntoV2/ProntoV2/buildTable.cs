c2f7e49a744a474c69082d01ce22f25f37933644
Yarden Leitner <yardenleitner@gmail.com>
2016-12-28 19:14:29 +0200

    hertut

--- ProntoV2/ProntoV2/buildTable.cs
+++ ProntoV2/ProntoV2/buildTable.cs
@@ -11,10 +11,11 @@ using Android.Views;
 using Android.Widget;
 using System.IO;
 using SQLite;
+using Java.IO;
 
 namespace ProntoV2
 {
-    class buildTable
+    public class buildTable
     {
         static string folder;
         static SQLiteConnection conn;
@@ -28,13 +29,15 @@ namespace ProntoV2
             //  conn.CreateTable<Products>();
         }
 
-        public static void Initialize()
+        public static void Initialize(InputStream r)
         {
             folder = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
             conn = new SQLiteConnection(System.IO.Path.Combine(folder, "myAmmazingApp.db"));
-            conn.CreateTable<Item>();
-            //conn.CreateTable<Product>();
-            //  conn.CreateTable<Products>(); }
+        }
+
+        public static string getDBPath()
+        {
+            return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), "myAmmazingApp.db");
         }
 
         public void Close() { conn.Close();folder = string.Empty; }
@@ -44,9 +47,9 @@ namespace ProntoV2
             Item itm1 = new Item("7296014048203", true, "tishu", "haznazuzi", "israel", "nice tishu", "pieces", 2, false, true, "gram", 24, 12, true, true);
             Item itm2 = new Item("013495113537", true, "klik", "elit", "israel", "klik white and brown", "100 g", 2, false, true, "gram", 10, (float)9.4, true, true);
             Item itm3 = new Item("729001370255", true, "schweps", "haznazuzi", "italy", "good shweps drink", "100 ml", 2, false, true, "ml", 12, 3, true, true);
-            buildTable.AddItem(conn, itm1);
-            buildTable.AddItem(conn, itm2);
-            buildTable.AddItem(conn, itm3);
+            buildTable.AddItem(itm1);
+            buildTable.AddItem(itm2);
+            buildTable.AddItem(itm3);
         }
 
         public static void AddItem(Item itm)
