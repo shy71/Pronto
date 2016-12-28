a77d31ca3957b3b0b9a521d54821176199b197d6
neryag <neryagr@gmail.com>
2016-12-28 22:30:31 +0200

    n

--- ProntoV2/ProntoV2/Resources/Resource.Designer.cs
+++ ProntoV2/ProntoV2/Resources/Resource.Designer.cs
@@ -44,6 +44,25 @@ namespace ProntoV2
 			}
 		}
 		
+		public partial class Dimension
+		{
+			
+			// aapt resource value: 0x7f040000
+			public const int button_font_size = 2130968576;
+			
+			// aapt resource value: 0x7f040001
+			public const int font_size = 2130968577;
+			
+			static Dimension()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Dimension()
+			{
+			}
+		}
+		
 		public partial class Drawable
 		{
 			
@@ -77,6 +96,9 @@ namespace ProntoV2
 			// aapt resource value: 0x7f020009
 			public const int ShoppingIcon = 2130837513;
 			
+			// aapt resource value: 0x7f02000a
+			public const int updownbuttons = 2130837514;
+			
 			static Drawable()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
@@ -90,101 +112,101 @@ namespace ProntoV2
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050009
-			public const int X = 2131034121;
+			// aapt resource value: 0x7f060009
+			public const int X = 2131099657;
 			
-			// aapt resource value: 0x7f05001e
-			public const int addBtn = 2131034142;
+			// aapt resource value: 0x7f06001e
+			public const int addBtn = 2131099678;
 			
-			// aapt resource value: 0x7f050014
-			public const int brand = 2131034132;
+			// aapt resource value: 0x7f060014
+			public const int brand = 2131099668;
 			
-			// aapt resource value: 0x7f05001d
-			public const int checkoutBtn = 2131034141;
+			// aapt resource value: 0x7f06001d
+			public const int checkoutBtn = 2131099677;
 			
-			// aapt resource value: 0x7f05001f
-			public const int contentFrame = 2131034143;
+			// aapt resource value: 0x7f06001f
+			public const int contentFrame = 2131099679;
 			
-			// aapt resource value: 0x7f050019
-			public const int creditCardNum = 2131034137;
+			// aapt resource value: 0x7f060019
+			public const int creditCardNum = 2131099673;
 			
-			// aapt resource value: 0x7f050013
-			public const int date = 2131034131;
+			// aapt resource value: 0x7f060013
+			public const int date = 2131099667;
 			
-			// aapt resource value: 0x7f05001b
-			public const int daySpinner = 2131034139;
+			// aapt resource value: 0x7f06001b
+			public const int daySpinner = 2131099675;
 			
-			// aapt resource value: 0x7f050000
-			public const int deleteButton = 2131034112;
+			// aapt resource value: 0x7f060000
+			public const int deleteButton = 2131099648;
 			
-			// aapt resource value: 0x7f050002
-			public const int description = 2131034114;
+			// aapt resource value: 0x7f060002
+			public const int description = 2131099650;
 			
-			// aapt resource value: 0x7f05000b
-			public const int detailsFood = 2131034123;
+			// aapt resource value: 0x7f06000b
+			public const int detailsFood = 2131099659;
 			
-			// aapt resource value: 0x7f050003
-			public const int foodCompany = 2131034115;
+			// aapt resource value: 0x7f060003
+			public const int foodCompany = 2131099651;
 			
-			// aapt resource value: 0x7f05000d
-			public const int foodIcon = 2131034125;
+			// aapt resource value: 0x7f06000d
+			public const int foodIcon = 2131099661;
 			
-			// aapt resource value: 0x7f05000c
-			public const int foodName = 2131034124;
+			// aapt resource value: 0x7f06000c
+			public const int foodName = 2131099660;
 			
-			// aapt resource value: 0x7f050001
-			public const int foodPic = 2131034113;
+			// aapt resource value: 0x7f060001
+			public const int foodPic = 2131099649;
 			
-			// aapt resource value: 0x7f050007
-			public const int foodQTY = 2131034119;
+			// aapt resource value: 0x7f060007
+			public const int foodQTY = 2131099655;
 			
-			// aapt resource value: 0x7f05000f
-			public const int historyBtn = 2131034127;
+			// aapt resource value: 0x7f06000f
+			public const int historyBtn = 2131099663;
 			
-			// aapt resource value: 0x7f050018
-			public const int idText = 2131034136;
+			// aapt resource value: 0x7f060018
+			public const int idText = 2131099672;
 			
-			// aapt resource value: 0x7f05000e
-			public const int imageView1 = 2131034126;
+			// aapt resource value: 0x7f06000e
+			public const int imageView1 = 2131099662;
 			
-			// aapt resource value: 0x7f050011
-			public const int main = 2131034129;
+			// aapt resource value: 0x7f060011
+			public const int main = 2131099665;
 			
-			// aapt resource value: 0x7f050006
-			public const int minusButton = 2131034118;
+			// aapt resource value: 0x7f060006
+			public const int minusButton = 2131099654;
 			
-			// aapt resource value: 0x7f05001a
-			public const int monthSpinner = 2131034138;
+			// aapt resource value: 0x7f06001a
+			public const int monthSpinner = 2131099674;
 			
-			// aapt resource value: 0x7f050008
-			public const int plusButton = 2131034120;
+			// aapt resource value: 0x7f060008
+			public const int plusButton = 2131099656;
 			
-			// aapt resource value: 0x7f050017
-			public const int previewDetail = 2131034135;
+			// aapt resource value: 0x7f060017
+			public const int previewDetail = 2131099671;
 			
-			// aapt resource value: 0x7f050016
-			public const int price = 2131034134;
+			// aapt resource value: 0x7f060016
+			public const int price = 2131099670;
 			
-			// aapt resource value: 0x7f050004
-			public const int pricePerUnit = 2131034116;
+			// aapt resource value: 0x7f060004
+			public const int pricePerUnit = 2131099652;
 			
-			// aapt resource value: 0x7f05000a
-			public const int qty = 2131034122;
+			// aapt resource value: 0x7f06000a
+			public const int qty = 2131099658;
 			
-			// aapt resource value: 0x7f05001c
-			public const int securityText = 2131034140;
+			// aapt resource value: 0x7f06001c
+			public const int securityText = 2131099676;
 			
-			// aapt resource value: 0x7f050012
-			public const int shopDetail = 2131034130;
+			// aapt resource value: 0x7f060012
+			public const int shopDetail = 2131099666;
 			
-			// aapt resource value: 0x7f050010
-			public const int shopNowButton = 2131034128;
+			// aapt resource value: 0x7f060010
+			public const int shopNowButton = 2131099664;
 			
-			// aapt resource value: 0x7f050015
-			public const int store = 2131034133;
+			// aapt resource value: 0x7f060015
+			public const int store = 2131099669;
 			
-			// aapt resource value: 0x7f050005
-			public const int totalPrice = 2131034117;
+			// aapt resource value: 0x7f060005
+			public const int totalPrice = 2131099653;
 			
 			static Id()
 			{
@@ -248,11 +270,14 @@ namespace ProntoV2
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f040001
-			public const int ApplicationName = 2130968577;
+			// aapt resource value: 0x7f050002
+			public const int ApplicationName = 2131034114;
 			
-			// aapt resource value: 0x7f040000
-			public const int Hello = 2130968576;
+			// aapt resource value: 0x7f050001
+			public const int Hello = 2131034113;
+			
+			// aapt resource value: 0x7f050000
+			public const int app_name = 2131034112;
 			
 			static String()
 			{
